8cad8c63b4db8116201ae88dcd8377600a6e7fc8
SirDancealot <mj@lpj-software.dk>
2019-03-13 17:58:33 +0100

    Finished the program
    With verry ugly code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		String dir1 = "RETR ubuntu/dists/artful-backports/Release.gpg\n";
-		//String dir2 = "mget ubuntu\\pool\\main\\a\\ally-profile-manager\\ally-profile-manager_0.1.10.orig.tar.xz\r\n";
+		String dir2 = "RETR /ubuntu/project/trace/sadashbia.canonical.com.dists-timestamps\n";
+		//String dir2 = "RETR ubuntu/pool/main/a/ally-profile-manager/ally-profile-manager_0.1.10.orig.tar.xz\n";
 
 		String hostname = "ftp.ubuntu.com";
 
@@ -14,7 +15,6 @@ public class Main {
 		Socket clientSocket = new Socket(hostname, port);
 
 		OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
-
 		InputStream in = clientSocket.getInputStream();
 
 		int ans = -2;
@@ -40,11 +40,30 @@ public class Main {
 		out.write("PASV\n");
 		out.flush();
 		ans = -2;
+		while (ans != (int)'(') {
+			ans = in.read();
+			System.out.print((char)ans);
+		}
+		String portInfo = "";
+		while (ans != (int)')') {
+			ans = in.read();
+			portInfo += (char)ans;
+			System.out.print((char)ans);
+		}
 		while (ans != (int)'\n') {
 			ans = in.read();
 			System.out.print((char)ans);
 		}
-		Socket clientSocketlisten = new Socket(hostname, port + 1);
+
+		String[] totalPortInfo = portInfo.substring(0,portInfo.length()-1).split(",");
+
+		String dataHost = totalPortInfo[0] + "." + totalPortInfo[1] + "." + totalPortInfo[2] + "." + totalPortInfo[3];
+
+		int dataPort = Integer.parseInt(totalPortInfo[4])*256 + Integer.parseInt(totalPortInfo[5]);
+
+		Socket dataSocket = new Socket(dataHost, dataPort);
+		InputStream dataIn = dataSocket.getInputStream();
+
 
 		out.write(dir1.toCharArray(), 0, dir1.length());
 		out.flush();
@@ -52,28 +71,108 @@ public class Main {
 
 		String path = System.getProperty("user.home")+"/downloads";
 		File file1 = new File(path+"/file1.txt");
-		File file2 = new File(path+"/file2.txt");
 
 		FileOutputStream os1 = new FileOutputStream(file1);
-		//FileOutputStream os2 = new FileOutputStream(file2);
 		int b = -2;
-		for (int i = 0; i < 1024 && b != (int)'\n'; i++) {
-			b = in.read();
-			os1.write(b);
-			System.out.print((char)b);
-		}
 		while (b != (int)'\n') {
 			b = in.read();
-			os1.write(b);
-			System.out.println((char)b);
+			System.out.print((char)b);
 		}
+		int bytesWritten = 0;
+		int charByte;
+		while (true) {
+			charByte = dataIn.read();
+			if (charByte == -1) {
+				charByte = 0;
+				break;
+			}
+			os1.write(charByte);
+			if (bytesWritten < 1024) {
+				System.out.print(Integer.toBinaryString(charByte));
+				bytesWritten++;
+			}
+		}
+		System.out.println();
 		os1.flush();
+		os1.close();
+		dataIn.close();
+		dataSocket.close();
 
 
 
-		clientSocket.close();
+		b = -2;
+		while (b != (int)'\n') {
+			b = in.read();
+			System.out.print((char)b);
+		}
+		System.out.println("File " + dir1 + " has been downloaded and saved to 'user/downloads/file1.txt'");
 
 
+		File file2 = new File(path+"/file2.txt");
+		FileOutputStream os2 = new FileOutputStream(file2);
+
+		out.write("PASV\n");
+		out.flush();
+
+		ans = -2;
+		while (ans != (int)'(') {
+			ans = in.read();
+			System.out.print((char)ans);
+		}
+		portInfo = "";
+		while (ans != (int)')') {
+			ans = in.read();
+			portInfo += (char)ans;
+			System.out.print((char)ans);
+		}
+		while (ans != (int)'\n') {
+			ans = in.read();
+			System.out.print((char)ans);
+		}
+
+		totalPortInfo = portInfo.substring(0,portInfo.length()-1).split(",");
+
+		dataHost = totalPortInfo[0] + "." + totalPortInfo[1] + "." + totalPortInfo[2] + "." + totalPortInfo[3];
+
+		dataPort = Integer.parseInt(totalPortInfo[4])*256 + Integer.parseInt(totalPortInfo[5]);
+
+		dataSocket = new Socket(dataHost, dataPort);
+		dataIn = dataSocket.getInputStream();
+
+
+		out.write(dir2.toCharArray(), 0, dir2.length());
+		out.flush();
+
+
+
+		b = -2;
+		while (b != (int)'\n') {
+			b = in.read();
+			System.out.print((char)b);
+		}
+		bytesWritten = 0;
+		while (true) {
+			charByte = dataIn.read();
+			if (charByte == -1) {
+				charByte = 0;
+				break;
+			}
+			os2.write(charByte);
+			if (bytesWritten < 1024) {
+				System.out.print(Integer.toBinaryString(charByte));
+				bytesWritten++;
+			}
+		}
+		System.out.println();
+		b = -2;
+		while (b != (int)'\n') {
+			b = in.read();
+			System.out.print((char)b);
+		}
+		os2.flush();
+		os2.close();
+		dataIn.close();
+		dataSocket.close();
 
 	}
 }
